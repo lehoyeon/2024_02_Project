f65a557ff273675d4e960ab9819f92858ecdc021
Yudir-98 <Wjsdbqja156@gmail.com>
2025-02-12 13:55:13 +0900

    correspondent_product upload

--- monmi/src/main/java/com/monmi/sales/repository/search/CorrespondentMaterialSearch.java
+++
@@ -1,9 +0,0 @@
-package com.monmi.sales.repository.search;
-
-import com.monmi.sales.domain.CorrespondentMaterial;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface CorrespondentMaterialSearch {
-    Page<CorrespondentMaterial> correspondent_material_search_all(String[] types, String keyword, Pageable pageable);
-}
